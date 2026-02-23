9b7c29697207c738f23a80f38aa818a9bc3e55e3
tejaswi <tejaswi0623@gmail.com>
2026-02-23 15:22:52 -0500

    msg

---
+++ mysql.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+userid=$(id -u)
+logs_folder="/var/log/shell-script"
+logs_file="$logs_folder/$0.log"
+R="\e[31m"
+G="\e[32m"
+N="\e0m"
+mongodb_host="mongodb.jarugula.online"
+
+if [ $userid -ne 0 ]; then
+   echo -e "$R please run the script with root user access $N" |tee -a $logs_file
+   exit 1
+fi
+
+validate(){
+    if [ $1 -ne 0 ]; then
+      echo -e "$2 is..........$R failed $N" |tee -a $logs_file
+      exit 1
+    else
+      echo -e "$2 is...........$G success $N" |tee -a $logs_file
+    fi
+}
+
+dnf install mysql-server -y &>>$logs_file
+validate $? "installing mysql"
+
+systemctl enable mysqld &>>$logs_file
+systemctl start mysqld
+validate $? "eanble and start mysql"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+validate $? "setting root password"
+
+mysql -h $mongodb_host -u root -p<password>
+validate $? "loading data"
